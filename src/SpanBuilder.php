7698743be72a3e47ffd28d8b7dc63a3e26ec3514
Shish <shish@shishnet.org>
2025-10-04 17:46:29 +0100

    better span::end

--- src/SpanBuilder.php
+++ src/SpanBuilder.php
@@ -11,6 +11,12 @@ use Opentelemetry\Proto\Trace\V1\Status\StatusCode;
 class SpanBuilder
 {
     protected Span $span;
+    protected string $id;
+
+    /**
+     * @var array<string, mixed> $attributes
+     */
+    protected ?array $attributes = null;
 
     /**
      * @param array<string, mixed> $attributes
@@ -20,28 +26,51 @@ class SpanBuilder
         string $name,
         array $attributes = [],
     ) {
-        $my_id = strtoupper(bin2hex(random_bytes(8)));
+        $this->id = strtoupper(bin2hex(random_bytes(8)));
+        $this->attributes = $attributes;
+
         $this->span = new Span([
             "trace_id" => base64_decode($this->client->traceId),
-            "span_id" => base64_decode($my_id),
+            "span_id" => base64_decode($this->id),
             "parent_span_id" => base64_decode(end($this->client->spanIds) ?: ''),
             "name" => $name,
             "start_time_unix_nano" => (string)(int)(microtime(true) * 1e9),
             //"end_time_unix_nano" => "0",
             "kind" => Span\SpanKind::SPAN_KIND_SERVER,
-            "attributes" => Client::dict2otel($attributes),
         ]);
-        $this->client->spanIds[] = $my_id;
+        $this->client->spanIds[] = $this->id;
     }
 
-    public function end(?bool $success = null): void
+    /**
+     * @param array<string, mixed> $attributes
+     */
+    public function end(?bool $success = null, ?string $message = null, ?array $attributes = null): void
     {
-        array_pop($this->client->spanIds);
-        if ($success !== null) {
+        // remove my spanId from the list, even if it's in the middle
+        $this->client->spanIds = array_values(array_filter(
+            $this->client->spanIds,
+            fn ($id) => $id !== $this->id
+        ));
+
+        $attrsToSet = $this->attributes;
+        if ($attributes !== null) {
+            $attrsToSet = array_merge($attrsToSet ?? [], $attributes);
+        }
+        if ($attrsToSet !== null) {
+            $this->span->setAttributes(Client::dict2otel($attrsToSet));
+        }
+
+        if ($success !== null || $message !== null) {
             $this->span->setStatus(new Status([
-                "code" => $success ? StatusCode::STATUS_CODE_OK : StatusCode::STATUS_CODE_ERROR,
+                "code" => match($success) {
+                    true => StatusCode::STATUS_CODE_OK,
+                    false => StatusCode::STATUS_CODE_ERROR,
+                    null => StatusCode::STATUS_CODE_UNSET,
+                },
+                "message" => $message,
             ]));
         }
+
         $this->span->setEndTimeUnixNano((string)(int)(microtime(true) * 1e9));
         $this->client->logSpan($this->span);
     }
