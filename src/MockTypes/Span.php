80aea090c6acefff0036a7dd5759cd850b0d2b83
Shish <shish@shishnet.org>
2025-10-07 14:56:31 +0100

    lint

--- src/MockTypes/Span.php
+++ src/MockTypes/Span.php
@@ -6,6 +6,9 @@ namespace MicroOTLP\MockTypes;
 
 class Span extends Message
 {
+    /**
+     * @param array<string, mixed> $attributes
+     */
     public function setAttributes(array $attributes): void
     {
         $this->props["attributes"] = $attributes;
